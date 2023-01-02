4ae68d78472bf79ce74c17e6fd6c624cc401f1a8
terry <tjordan@jordant.com>
2023-01-01 20:53:51 -0700

    MODULE update for GO | Sunday - Jan 01, 2023 @ 20:53

--- Gadgets/AWS/awsMAIN.go
+++ Gadgets/AWS/awsMAIN.go
@@ -7,11 +7,17 @@ package CUSTOM_GO_MODULE
 import (
 	
 	"context"
-
-	. "github.com/acedev0/GOGO/Gadgets"
+    "time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/dynamodb"    
+    "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"     
+    "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"   
+
+
+    . "github.com/acedev0/GOGO/Gadgets"
+
 )
 
 
@@ -19,7 +25,17 @@ var AWS_REGION = "us-east-1"
 var AWS_PROFILE = "terry"
 var AWS_CONF_SESS aws.Config
 
+var DYNAMO_TABLE = "toolchain_requestsXXXXX"
+var DYNAMO_SVC  *dynamodb.Client
+
+var AWS_ALREADY_INIT = false
+
 func AWS_INIT() {
+
+
+    if AWS_ALREADY_INIT {
+        return
+    }
 
     C.Println(" **| Connecting to AWS ")
 
@@ -56,8 +72,100 @@ cfg, err := config.LoadDefaultConfig(
 	// Otherwsie save the CONF object so we can use it later
 	AWS_CONF_SESS = cfg
 
+    AWS_ALREADY_INIT = true
 
-	// also lets init other AWS tools (S3 and Dynamodb)
-	//S3_INIT()
-	//Dynamo_INIT()
+}
+
+
+
+func DYNAMO_INIT() {
+    
+    SHOW_BOX(" Connecting to DYNAMO ..")
+
+    AWS_INIT()
+
+    DYNAMO_SVC = dynamodb.NewFromConfig(AWS_CONF_SESS)
+	
+}
+
+
+
+
+
+func DYN_CreateTable(tableName string) {
+
+	var PRIMARY_KEY_NAME = "id"
+
+	C.Print(" - -| Trying to Create Dynamo Table: ")
+	Y.Println(tableName)
+	C.Println(" Remember PRimary Key is always: ", PRIMARY_KEY_NAME)
+
+    _, err := DYNAMO_SVC.CreateTable(context.TODO(), &dynamodb.CreateTableInput{ 
+        TableName:   aws.String(tableName),
+        BillingMode: types.BillingModePayPerRequest,
+
+		// ProvisionedThroughput: &types.ProvisionedThroughput{
+		// 	ReadCapacityUnits:  aws.Int64(PartitionWriteReadCap),
+		// 	WriteCapacityUnits: aws.Int64(PartitionWriteReadCap),
+		// },		
+        KeySchema: []types.KeySchemaElement{
+            {
+                AttributeName: aws.String(PRIMARY_KEY_NAME),
+                KeyType:       types.KeyTypeHash,
+            },
+        },
+        AttributeDefinitions: []types.AttributeDefinition{
+            {
+                AttributeName: aws.String(PRIMARY_KEY_NAME),
+                AttributeType: types.ScalarAttributeTypeS,
+            },
+        },		
+    })
+    if err != nil {
+		M.Println(" Cant Create Table: ", err)
+		return
+    }
+
+	// Now wait for table creation
+	Y.Println(" - -| Now Waiting for Table to be created...")
+
+	w := dynamodb.NewTableExistsWaiter(DYNAMO_SVC)
+    err = w.Wait(context.TODO(),
+        &dynamodb.DescribeTableInput{
+            TableName: aws.String(tableName),
+        },
+        2*time.Minute,
+        func(o *dynamodb.TableExistsWaiterOptions) {
+            o.MaxDelay = 5 * time.Second
+            o.MinDelay = 5 * time.Second
+        })
+    if err != nil {
+		M.Print(" - -| Timed out waiting for table! ")
+		Y.Println(err)
+    }
+}
+
+
+
+func DYN_InsertItem(tableName string, item interface{} )  {
+
+	SHOW_BOX("**| About to Perform Dynamo INSERT on:", "|white|" + tableName )
+	
+	data, err := attributevalue.MarshalMap(item)
+
+	if err != nil {
+		M.Println("DYN Insert error", err)
+        return
+	}
+
+	_, err = DYNAMO_SVC.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      data,
+	})
+
+	if err != nil {
+		M.Println("DYN Insert error", err, err.Error())
+        return		
+	}
+	G.Println(" DYN Insert Success")
 }
--- Gadgets/AWS/go.mod
+++ Gadgets/AWS/go.mod
@@ -3,30 +3,38 @@ module github.com/acedev0/GOGO/Gadgets/AWS
 go 1.19
 
 require (
-	github.com/acedev0/GOGO/Gadgets v0.0.0-20221130235243-d7910476f9f9
-	github.com/aws/aws-sdk-go-v2 v1.17.2
-	github.com/aws/aws-sdk-go-v2/config v1.18.4
+	github.com/acedev0/GOGO/Gadgets v0.0.0-20230102033136-0ffb483cc74a
+	github.com/aws/aws-sdk-go-v2 v1.17.3
+	github.com/aws/aws-sdk-go-v2/config v1.18.7
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.8
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.9
 )
 
 require (
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.4 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.20 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.26 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.20 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.27 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.20 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.11.26 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.9 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.17.6 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.7 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.7 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
-	github.com/briandowns/spinner v1.19.0 // indirect
+	github.com/briandowns/spinner v1.20.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/janeczku/go-spinner v0.0.0-20150530144529-cf8ef1d64394 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/crypto v0.3.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/term v0.2.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
 )
